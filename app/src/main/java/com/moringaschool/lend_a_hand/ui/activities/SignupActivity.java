d98a64b8322061b9434b6208921b2fe5095585a8
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-20 16:46:01 +0300

    Initial commit

---
+++ app/src/main/java/com/moringaschool/lend_a_hand/ui/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.lend_a_hand.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.lend_a_hand.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
