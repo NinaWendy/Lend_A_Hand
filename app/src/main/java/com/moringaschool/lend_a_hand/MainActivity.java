d9ee66a6d0de2459fb4ed11fce52a4075365ef85
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-23 00:43:15 +0300

    Add splash screen

--- app/src/main/java/com/moringaschool/lend_a_hand/MainActivity.java
+++ app/src/main/java/com/moringaschool/lend_a_hand/MainActivity.java
@@ -1,14 +1,28 @@
 package com.moringaschool.lend_a_hand;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.moringaschool.lend_a_hand.ui.activities.CheckInActivity;
 
 public class MainActivity extends AppCompatActivity {
-//Development
+
+    Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, CheckInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
