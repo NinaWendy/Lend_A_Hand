d10e9bb1593ea49ea827a74f84f3d91d9d85366b
jumaBrian <njengabrianj@gmail.com>
2022-06-22 17:34:23 +0300

    ft: Add organizations to firebase realtime database from organization add form

--- app/src/main/java/com/moringaschool/lend_a_hand/MainActivity.java
+++ app/src/main/java/com/moringaschool/lend_a_hand/MainActivity.java
@@ -2,13 +2,32 @@ package com.moringaschool.lend_a_hand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.moringaschool.lend_a_hand.ui.activities.LoginActivity;
+import com.moringaschool.lend_a_hand.ui.activities.OrgActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.mainTextView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OrgActivity.class));
+            }
+        });
+
+
     }
 }
