0091a6f70da91ab914eccca5fcfe3edb6eafc05c
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-23 00:44:21 +0300

    Add check in activity with lottie animation

--- app/src/main/java/com/moringaschool/lend_a_hand/ui/activities/CheckInActivity.java
+++ app/src/main/java/com/moringaschool/lend_a_hand/ui/activities/CheckInActivity.java
@@ -2,15 +2,46 @@ package com.moringaschool.lend_a_hand.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.lend_a_hand.R;
 
-public class CheckInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class CheckInActivity extends AppCompatActivity {
+    @BindView(R.id.charitySignup)
+    TextView charity;
+    @BindView(R.id.usrLogin) TextView login;
+    @BindView(R.id.email) TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+        ButterKnife.bind(this);
+        charity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckInActivity.this, OrgActivity.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckInActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckInActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
